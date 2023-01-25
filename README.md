7bc3060d3d84df6ae68644aaa1d2dfb5ba7c4535
unknown <dcanellesgomez@gmail.com>
2023-01-25 11:51:10 -0500

    add readme and some comments

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rest-api-tester
+<h3>Installation Guide:</h3>
+<p>Go to the root project and run:</p>
+<code>pip install -r requirements.txt</code>
+<p>Make sure do you have installed python at least 3.10.0. Add the url of your postgres database to the file database.py and this is all.</p>
+<p>Start de project running:</p>
+<code>uvicorn main:app --reload</code>
+
+<h3>Test Guide:</h3>
+<p>The app in now running in the port 8000 go to (http://localhost:8000/docs#/) and test all the endpoints using the swagger.</p>
--- utils.py
+++ utils.py
@@ -4,12 +4,22 @@ def test_request(request: schemas.Request):
     import requests
     import json
     passed = False
+
+    """
+        Here we create the params and header objects from the list of key values that the client provide.
+    """
+
     headers_object = {}
     params_object = {}
     for header in request.body['headers']:
         headers_object[header['key']] = header['value']
     for param in request.body['params']:
         params_object[param['key']] = param['value']
+
+    """
+        Here we identify wich method is define in the request and execute the correct function
+    """
+
     if request.body['method'] == 'GET':
         test = requests.get(request.url, params=params_object, headers=headers_object)
     elif request['method'] == 'POST':
@@ -20,12 +30,22 @@ def test_request(request: schemas.Request):
         test = requests.patch(request.url, data=request.body['body'], params=params_object, headers=headers_object)
     elif request.body['method'] == 'DELETE':
         test = requests.delete(request.url, data=request.body['body'], params=params_object, headers=headers_object)
+
+    """
+        Here extract the data from the response object (for now we only save json responses).
+    """
+
     response_data = test.content
     status = test.status_code
     try:
         response_data_json = json.loads(response_data)
     except:
         response_data_json = {}
+
+    """
+        Here perform the seach in the response of the search params.
+    """
+
     for search_param in request.seach_params:
         if search_param['key'] == "status":
             if int(search_param['value']) == status:
